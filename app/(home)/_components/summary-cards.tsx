36a511c816675b2ac9c5a2a3ef002f7e3929e6ed
MatheusSouzaDev <1002matheussouza@gmail.com>
2025-05-20 21:32:08 -0300

    feat: ensure user can not create transaction on free plan if above limit

--- app/(home)/_components/summary-cards.tsx
+++ app/(home)/_components/summary-cards.tsx
@@ -11,6 +11,7 @@ interface summaryCardsProps {
   depositTotal: number;
   expensesTotal: number;
   investmentsTotal: number;
+  userCanAddTransaction?: boolean;
 }
 
 const SummaryCards = async ({
@@ -18,6 +19,7 @@ const SummaryCards = async ({
   depositTotal,
   expensesTotal,
   investmentsTotal,
+  userCanAddTransaction,
 }: summaryCardsProps) => {
   return (
     <div className="space-y-6">
@@ -26,6 +28,7 @@ const SummaryCards = async ({
         title="Saldo"
         amount={balanceTotal}
         size="large"
+        userCanAddTransaction={userCanAddTransaction}
       />
       <div className="grid grid-cols-3 gap-6">
         <SummaryCard
