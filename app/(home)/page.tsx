717c8c14cd6d54232ddfcca59c850812ac36c554
MatheusSouzaDev <1002matheussouza@gmail.com>
2025-05-26 12:10:14 -0300

    feat: ensure only plus users can generate AI Report

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,4 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import Navbar from "../_components/navbar";
 import SummaryCards from "./_components/summary-cards";
@@ -21,6 +21,7 @@ const Home = async ({ searchParams }: { searchParams: { month?: string } }) => {
     month: currentMonth,
   });
   const userCanAddTransaction = await canUserAddTransaction();
+  const user = await clerkClient.users.getUser(userId);
 
   return (
     <>
@@ -29,7 +30,10 @@ const Home = async ({ searchParams }: { searchParams: { month?: string } }) => {
         <div className="flex justify-between">
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <div className="flex items-center gap-3">
-            <AiReportButton month={currentMonth} />
+            <AiReportButton
+              month={currentMonth}
+              hasPlusPlan={user.publicMetadata.subscriptionPlan == "plus"}
+            />
             <TimeSelect month={currentMonth} />
           </div>
         </div>
