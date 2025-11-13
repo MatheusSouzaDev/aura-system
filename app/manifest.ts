96d786b729666990b848a5811a4744eabb88a61e
matheussouzadev <1002matheussouza@gmail.com>
2025-11-13 16:31:02 -0300

    fix: restyle android icon

--- app/manifest.ts
+++ app/manifest.ts
@@ -8,8 +8,8 @@ export default function manifest(): MetadataRoute.Manifest {
       "Aura System AI ajuda a monitorar movimentações financeiras e gerar insights com IA.",
     start_url: "/",
     display: "standalone",
-    background_color: "#050505",
-    theme_color: "#050505",
+    background_color: "#000000",
+    theme_color: "#000000",
     icons: [
       {
         src: "/IconAurian.svg",
